8e7f5cc5c13f0d563a481c058162d9213d2b067d
karan <bkaransingh001@gmail.com>
2024-02-11 20:25:22 +0530

    first commit

---
+++ src/main/java/com/odafa/cloudapp/configuration/WebSocketConfiguration.java
@@ -0,0 +1,43 @@
+package com.odafa.cloudapp.configuration;
+
+import com.odafa.cloudapp.controller.VideoSocketHandler;
+
+import io.micrometer.common.lang.NonNull;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfiguration implements WebSocketConfigurer {
+	
+    private final VideoSocketHandler videoSocketHandler;
+	private final ConfigReader configurations;
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxBinaryMessageBufferSize(1024000);
+        return container;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(videoSocketHandler, configurations.getVideoWsEndpoint()).setAllowedOrigins("*");
+    }
+ 
+
+}
+
+
+
